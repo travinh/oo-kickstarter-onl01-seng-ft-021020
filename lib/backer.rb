0be95687f437d8a0466391a8d9cbe0b9aae42671
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-26 03:44:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,11 @@
 class Backer
+  attr_accessor :name
+  attr_reader :backed_projects
   
-  attr_reader :backed_project
-  
-  def initialize(backed_project)
-    @backed_project = backed_project
+  def initialize(name)
+    @name = name
     @backed_projects = []
+    
     
   end
   
